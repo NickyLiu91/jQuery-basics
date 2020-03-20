763124e3c84b181adb3cb4f2b0447beeae701e87
Nicky <nickyliu91@gmail.com>
2020-03-19 22:15:39 -0400

    added back all old functions as commented out

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 $(document).ready(() => {
+
+  // $("button").click(function(){console.log("hi")})
+
+  // $("#button2").click(function(){
+  //   $("p").hide()
+  // })
+
+  // $("#button3").click(function(){
+  //   $("p").show()
+  // })
+  //
+  // $("#button4").click(function(){
+  //   $("p").toggle()
+  // })
+
+  // $("#button5").click(() => {console.log($("#paragraph5").text())})
+  //
+  // $("#button6").click(function(){
+  //     $("#paragraph5").text("ph'nglui mglw'nafh Cthulhu R'lyeh   wgah'nagl fhtagn")
+  // })
+
   $("#button1").click(function(){
     $("#paragraph1").prepend("THE BEGINNING")
   })
