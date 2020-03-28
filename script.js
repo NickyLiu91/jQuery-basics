285d1700c18cc7505f25472421273e320fcf154d
Nicky <nickyliu91@gmail.com>
2020-03-28 07:49:14 -0400

    rearranged functions

--- script.js
+++ script.js
@@ -14,11 +14,21 @@ $(document).ready(() => {
   //   $("p").toggle()
   // })
 
-  // $("#button5").click(() => {console.log($("#paragraph5").text())})
-  //
-  // $("#button6").click(function(){
-  //     $("#paragraph5").text("ph'nglui mglw'nafh Cthulhu R'lyeh   wgah'nagl fhtagn")
-  // })
+  $("#button1").click(() => {console.log($("#paragraph5").text())})
+
+  $("#button2").click(function(){
+      $("#paragraph5").text("ph'nglui mglw'nafh Cthulhu R'lyeh   wgah'nagl fhtagn")
+  })
+
+  $("#button3").click(function(){
+    console.log($("#paragraph1").attr('class'))
+  })
+
+  $("#button4").click(function(){
+    console.log($("#paragraph5").attr('class'))
+    $("#paragraph5").attr('class', 'classX');
+    console.log($("#paragraph5").attr('class'))
+  })
 
   // $("#button1").click(function(){
   //   $("#paragraph1").prepend("THE BEGINNING")
@@ -45,22 +55,13 @@ $(document).ready(() => {
   //   $("#paragraph1").empty();
   // })
 
-  $("#button1").click(function(){
-    console.log($("#paragraph1").attr('class'))
-  })
 
-  $("#button2").click(function(){
-    console.log($("#paragraph5").attr('class'))
-    $("#paragraph5").attr('class', 'classX');
-    console.log($("#paragraph5").attr('class'))
-  })
-
-  $("#button3").click(function(){
-    $("#paragraph1").css('color', 'red');
-  })
-
-  $("#button4").click(function(){
-    $("#paragraph2").css({'color': 'red', 'background-color': 'blue'});
-  })
+  // $("#button3").click(function(){
+  //   $("#paragraph1").css('color', 'red');
+  // })
+  //
+  // $("#button4").click(function(){
+  //   $("#paragraph2").css({'color': 'red', 'background-color': 'blue'});
+  // })
 
 })
