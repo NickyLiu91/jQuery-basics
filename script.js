81a954fb0a36e578aedc797f06155fc1ddf6b5ef
Nicky <nickyliu91@gmail.com>
2020-03-28 07:57:44 -0400

    added class specific functions

--- script.js
+++ script.js
@@ -14,21 +14,21 @@ $(document).ready(() => {
   //   $("p").toggle()
   // })
 
-  $("#button1").click(() => {console.log($("#paragraph5").text())})
-
-  $("#button2").click(function(){
-      $("#paragraph5").text("ph'nglui mglw'nafh Cthulhu R'lyeh   wgah'nagl fhtagn")
-  })
-
-  $("#button3").click(function(){
-    console.log($("#paragraph1").attr('class'))
-  })
-
-  $("#button4").click(function(){
-    console.log($("#paragraph5").attr('class'))
-    $("#paragraph5").attr('class', 'classX');
-    console.log($("#paragraph5").attr('class'))
-  })
+  // $("#button1").click(() => {console.log($("#paragraph5").text())})
+  //
+  // $("#button2").click(function(){
+  //     $("#paragraph5").text("ph'nglui mglw'nafh Cthulhu R'lyeh   wgah'nagl fhtagn")
+  // })
+  //
+  // $("#button3").click(function(){
+  //   console.log($("#paragraph1").attr('class'))
+  // })
+  //
+  // $("#button4").click(function(){
+  //   console.log($("#paragraph5").attr('class'))
+  //   $("#paragraph5").attr('class', 'classX');
+  //   console.log($("#paragraph5").attr('class'))
+  // })
 
   // $("#button1").click(function(){
   //   $("#paragraph1").prepend("THE BEGINNING")
@@ -56,12 +56,23 @@ $(document).ready(() => {
   // })
 
 
-  // $("#button3").click(function(){
-  //   $("#paragraph1").css('color', 'red');
-  // })
-  //
-  // $("#button4").click(function(){
-  //   $("#paragraph2").css({'color': 'red', 'background-color': 'blue'});
-  // })
+  $("#button1").click(function(){
+      console.log($("#paragraph5").attr('class'))
+      $("#paragraph5").addClass('classX');
+      console.log($("#paragraph5").attr('class'))
+  })
+
+  $("#button2").click(function(){
+    console.log($("#paragraph5").attr('class'))
+    $("#paragraph5").removeClass('classX');
+    console.log($("#paragraph5").attr('class'))
+  })
+  $("#button3").click(function(){
+    $("#paragraph1").css('color', 'red');
+  })
+
+  $("#button4").click(function(){
+    $("#paragraph2").css({'color': 'red', 'background-color': 'blue'});
+  })
 
 })
